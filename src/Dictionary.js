b4b7962bfa2ade6d7c8c987a4b3daa18d75bfd5e
standarthoid <90977509+standarthoid@users.noreply.github.com>
2022-08-27 12:08:45 +0200

    deleted alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,6 @@ export default function Dictionary() {
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for Jesus and ${keyword}`);
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);

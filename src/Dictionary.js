bd8de4a38c0ed532f57edf6f25ad23897ab0ab00
standarthoid <90977509+standarthoid@users.noreply.github.com>
2022-08-27 10:41:50 +0200

    added documentation description

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,6 +9,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for Jesus and ${keyword}`);
+    // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
